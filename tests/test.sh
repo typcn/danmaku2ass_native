b0552a157613ea807e5e82946d2c5e024f88865b
TYPCN <admin@typcn.com>
2015-04-08 13:16:58 +0800

    gcov ver fix

--- tests/test.sh
+++ tests/test.sh
@@ -2,4 +2,4 @@
 lcov --directory . --capture --output-file coverage.info
 lcov --remove coverage.info 'tests/*' '/usr/*' 'rapid*' --output-file coverage.info
 lcov --list coverage.info
-coveralls-lcov --repo-token '$COVERALLS_TOKEN' coverage.info
+coveralls-lcov --repo-token $COVERALLS_TOKEN coverage.info
