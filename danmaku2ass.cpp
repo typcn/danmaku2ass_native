b9c3dfa26b68a6959305c8f5c53dc774071e4c5e
TYPCN <admin@typcn.com>
2015-04-08 11:31:12 +0800

    Ass ♂ head write

--- danmaku2ass.cpp
+++ danmaku2ass.cpp
@@ -7,6 +7,7 @@
 
 #include "rapidxml/rapidxml.hpp"
 #include "rapidxml/rapidxml_utils.hpp"
+#include "danmaku2ass.h"
 
 using namespace std;
 using namespace rapidxml;
@@ -40,8 +41,42 @@ int GetCommentType(string headline){
     }
 }
 
+int round_int( double r ) {
+    return (r > 0.0) ? (r + 0.5) : (r - 0.5);
+}
+
 bool ConvertBilibiliComment(const char *xml,const char *outfile,int width,int height,const char *font,float fontsize,float alpha,float duration_marquee,float duration_still){
     std::ofstream out(outfile);
+    
+    // Write a♂ss head info
+    out << AssHead << endl;
+    out << "PlayResX: " << width << endl;
+    out << "PlayResY: " << height << endl;
+    out << "Aspect Ratio: " << width << ":" << height << endl;
+    out << "Collisions: Normal" << endl;
+    out << "WrapStyle: 2" << endl;
+    out << "ScaledBorderAndShadow: yes" << endl;
+    out << "YCbCr Matrix: TV.601" << endl << endl;
+
+    
+    // Get alpha in hex
+    char alpha_hex[3];
+    sprintf(alpha_hex, "%02X", 255-round_int(alpha*255));
+    
+    // Write ass styles , maybe disorder , See https://en.wikipedia.org/wiki/SubStation_Alpha
+    out << "[V4+ Styles]"<< endl;
+    out << "Format: Name, Fontname, Fontsize, PrimaryColour, SecondaryColour, OutlineColour, BackColour, Bold, Italic, Underline, StrikeOut, ScaleX, ScaleY, Spacing, Angle, BorderStyle, Outline, Shadow, Alignment, MarginL, MarginR, MarginV, Encoding"<< endl;
+    out << "Style: Danmaku2ASS_3c1a, " << // Style name
+        font << ", " << // Font name
+        fontsize << ", " << // Font size
+        "&H"<< alpha_hex <<"FFFFFF, " << // Primary Color
+        "&H"<< alpha_hex <<"FFFFFF, " << // Secondary Color
+        "&H"<< alpha_hex <<"000000, " << // Outline Color
+        "&H"<< alpha_hex <<"000000, " << // Back Color
+        "0, 0, 0, 0, 100, 100, 0.00, 0.00, 1, 1, 0, 7, 0, 0, 0, 0" << endl << endl;
+    
+    out << "[Events]" << endl;
+    out << "Format: Layer, Start, End, Style, Name, MarginL, MarginR, MarginV, Effect, Text" << endl;
     
     rapidxml::file<> xmlFile(xml);
     rapidxml::xml_document<> doc;
