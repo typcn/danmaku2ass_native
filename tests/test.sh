f88c67ee386b80ceea83a854217dabc2d6f2a45c
TYPCN <admin@typcn.com>
2015-09-13 21:57:39 +0800

    fix lcov dir

--- tests/test.sh
+++ tests/test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 ./danmaku2ass_native -in=../tests/testdata.xml -out=./test.out -w=1280 -h=720 -font="Heiti SC" -fontsize=25 -alpha=0.8 -dm=5 -ds=5
-$HOME/lcov/bin/lcov --gcov-tool "gcov-4.8" --directory . --capture --output-file coverage.info
-$HOME/lcov/bin/lcov --gcov-tool "gcov-4.8" --remove coverage.info 'tests/*' '/usr/*' 'rapid*' --output-file coverage.info
-$HOME/lcov/bin/lcov --gcov-tool "gcov-4.8" --list coverage.info
+$HOME/lcov/usr/bin/lcov --gcov-tool "gcov-4.8" --directory . --capture --output-file coverage.info
+$HOME/lcov/usr/bin/lcov --gcov-tool "gcov-4.8" --remove coverage.info 'tests/*' '/usr/*' 'rapid*' --output-file coverage.info
+$HOME/lcov/usr/bin/lcov --gcov-tool "gcov-4.8" --list coverage.info
 coveralls-lcov --repo-token $COVERALLS_TOKEN coverage.info
